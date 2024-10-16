21e9adfdd8a0a0c768546dfd0ff704834ed06340
diego <diegoonate3026@gmail.com>
2024-10-15 23:51:20 -0500

    96:Creating CRUD to type questions and aggregate relationships in question entity

--- src/main/java/com/survey/api/repositories/TypeQuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.survey.api.repositories;
-
-import com.survey.api.models.entities.TypeQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeQuestionRepository extends JpaRepository<TypeQuestion, Long> {
-}
