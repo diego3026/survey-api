07b479ed77ef93506f43864b3e6e0354c8ad8ff9
diego <diegoonate3026@gmail.com>
2024-10-15 23:44:27 -0500

    97:Creating CRUD to type questions and aggregate relationships in question entity

---
+++ src/main/java/com/survey/api/models/dtos/save/TypeQuestionRequest.java
@@ -0,0 +1,12 @@
+package com.survey.api.models.dtos.save;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TypeQuestionRequest {
+    @NotBlank
+    private String name;
+}
---
+++ src/main/java/com/survey/api/repositories/TypeQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.survey.api.repositories;
+
+import com.survey.api.models.entities.TypeQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeQuestionRepository extends JpaRepository<TypeQuestion, Long> {
+}
